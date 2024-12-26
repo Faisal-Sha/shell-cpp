07d5fd8b6f4aa9e4c17e1317390a70893087f423
faisal shahzad <faisal712000@gmail.com>
2024-12-26 12:29:17 +0500

    third task

--- src/main.cpp
+++ src/main.cpp
@@ -4,11 +4,13 @@ int main() {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
+  while (true)
+  {
+    // Uncomment this block to pass the first stage
+    std::cout << "$ ";
 
-  // Uncomment this block to pass the first stage
-  std::cout << "$ ";
-
-  std::string input;
-  std::getline(std::cin, input);
-  std::cout << input << ": command not found\n";
+    std::string input;
+    std::getline(std::cin, input);
+    std::cout << input << ": command not found\n";
+  }
 }
