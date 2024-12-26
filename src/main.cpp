4805d2ad3c6b600103ef047eb59f4ae8b6e7b9ef
faisal shahzad <faisal712000@gmail.com>
2024-12-26 13:04:14 +0500

    type command

--- src/main.cpp
+++ src/main.cpp
@@ -11,10 +11,18 @@ int main() {
 
     std::string input;
     std::getline(std::cin, input);
-    if(input=="exit 0") return 0;
-    if(input.find("echo ")==0) {
+    if(input=="exit 0") {
+      return 0;
+    } else if(input.find("echo ")==0) {
       std::string text = input.substr(5);
       std::cout << text << "\n";
+    } else if(input.find("type ")==0) {
+      std::string text = input.substr(5);
+      if(text.substr(0,4) == "type" || text.substr(0,4) == "exit" || text.substr(0,4) == "echo") {
+        std::cout << text << " is a shell builtin\n";
+      } else {
+        std::cout << text << " not found\n";
+      }
     } else {
       std::cout << input << ": command not found\n";
     }
