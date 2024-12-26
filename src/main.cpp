6248d4600f8a6d6f929c18006b2ba34c341054d2
faisal shahzad <faisal712000@gmail.com>
2024-12-26 13:30:07 +0500

    type path command

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,27 @@
 #include <iostream>
+#include <string>
+#include <sstream>
+#include <filesystem>
+
+
+std::string get_path(std::string cmd) {
+  std::string path_env = std::getenv("PATH");
+
+  std::stringstream ss(path_env);
+  std::string path;
+
+  while (!ss.eof())
+  {
+    getline(ss, path, ':');
+    std::string abs_path = path + "/" + cmd;
+    if (std::filesystem::exists(abs_path))
+    {
+      return abs_path;
+    }
+  }
+  
+  return "";
+}
 
 int main() {
   // Flush after every std::cout / std:cerr
@@ -21,7 +44,12 @@ int main() {
       if(text.substr(0,4) == "type" || text.substr(0,4) == "exit" || text.substr(0,4) == "echo") {
         std::cout << text << " is a shell builtin\n";
       } else {
-        std::cout << text << " not found\n";
+        std::string path = get_path(text);
+        if(path.empty()) {
+          std::cout << text << ": not found\n";
+        } else {
+          std::cout<< text.substr(5) << " is " << path << "\n";
+        }
       }
     } else {
       std::cout << input << ": command not found\n";
